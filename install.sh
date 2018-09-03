fdae340805fa55f134eb49f8a1dc794199586262
StarryLeo <starryskymayuyu@gmail.com>
2018-09-03 20:45:07 +0800

    For vim-plug

--- install.sh
+++ install.sh
@@ -16,13 +16,10 @@ else
 fi
 
 # Copy all fonts to user fonts directory
-echo "Copying fonts..."
 find "$consya_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
 
 # Reset font cache on Linux
 if which fc-cache >/dev/null 2>&1 ; then
-    echo "Resetting font cache, this may take a moment..."
     fc-cache -f "$font_dir"
 fi
 
-echo "Consolas-with-Yahei fonts installed to $font_dir"
